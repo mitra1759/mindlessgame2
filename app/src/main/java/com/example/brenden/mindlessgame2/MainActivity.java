4367e8794b8a8f59fd3bc8833a1e40af8bfd4066
mitra1759 <42783913+mitra1759@users.noreply.github.com>
2018-09-17 11:49:37 -0500

    added new features

--- app/src/main/java/com/example/brenden/mindlessgame2/MainActivity.java
+++ app/src/main/java/com/example/brenden/mindlessgame2/MainActivity.java
@@ -8,16 +8,17 @@ import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.os.CountDownTimer;
 import android.app.Application;
 import android.media.MediaPlayer;
+import java.lang.Object;
+
 
 public class MainActivity extends AppCompatActivity {
-    //    private Button start;
-//    private ImageView pendulum;
-    private void moverPendulum(){
-
-    }
+    int points=0;
+    int speed=1000;
+    private final static int MAX_VOLUME = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,18 +27,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final Button start = findViewById(R.id.button2);
         final ImageView pendulum = findViewById(R.id.imageView1);
+        final TextView score = findViewById(R.id.textView);
         final MediaPlayer ding = MediaPlayer.create(this, R.raw.ding);
-//        start.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                System.out.println("dwhsgd");
-//                pendulum.setImageResource(R.drawable.pendulum);
-//            }
-//        });
+        final MediaPlayer music = MediaPlayer.create(this, R.raw.background);
+        music.setLooping(true);
+        final float volume = (float) (1 - (Math.log(MAX_VOLUME - 13) / Math.log(MAX_VOLUME)));
+        music.setVolume(volume, volume);
+        music.start();
+        final RotateAnimation rotate = new RotateAnimation(-50, 1200, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_PARENT, 0f);
+        rotate.setDuration(700);
+        rotate.setInterpolator(new AccelerateDecelerateInterpolator());
+        rotate.setRepeatCount(0);
+        final RotateAnimation rotate1 = new RotateAnimation(50, -50, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_PARENT, 0f);
+        rotate1.setDuration(700);
+        rotate1.setInterpolator(new AccelerateDecelerateInterpolator());
+        rotate1.setRepeatCount(0);
+
 
         pendulum.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 if(pendulum.getRotation()<20&&pendulum.getRotation()>-20){
                     ding.start();
+                    points+=100;
+                    points*=1.3;
+                    points/=10;
+                    points*=10;
+                    score.setText("Score:"+points);
+                    speed*=1.03;
+                }
+                else {
+                    speed=1000;
+                    points=0;
+                    score.setText("Score:"+points);
                 }
             }
         });
@@ -63,28 +84,35 @@ public class MainActivity extends AppCompatActivity {
 //                pendulum.startAnimation(rotate1);
 
 
+//                pendulum.startAnimation(rotate1);
 
-                pendulum.animate().rotation(pendulum.getRotation() + 100).setInterpolator(new AccelerateDecelerateInterpolator()).setDuration(500).start();
-                new CountDownTimer(700, 700) {
 
-                    public void onTick(long millisUntilFinished) {
+                    pendulum.animate().rotation(pendulum.getRotation() + 100).setInterpolator(new AccelerateDecelerateInterpolator()).setDuration(1500000/speed).start();
+                    new CountDownTimer(1800000/speed, 600000/speed) {
+
+                        public void onTick(long millisUntilFinished) {
 //                        mTextField.setText("seconds remaining: " + millisUntilFinished / 1000);
-                    }
+                        }
 
-                    public void onFinish() {
-                        pendulum.animate().rotation(pendulum.getRotation() - 100).setInterpolator(new AccelerateDecelerateInterpolator()).setDuration(500).start();
-                    }
-                }.start();
-                new CountDownTimer(700, 700) {
+                        public void onFinish() {
+                            pendulum.animate().rotation(pendulum.getRotation() - 100).setInterpolator(new AccelerateDecelerateInterpolator()).setDuration(1500000/speed).start();
+//                            pendulum.RotateAnimation(pendulum.getRotation() ,pendulum.getRotation(),pendulum.getRotation(),pendulum.getRotation()).setInterpolator(new AccelerateDecelerateInterpolator()).setDuration(500).start();
+//                            pendulum.startAnimation(rotate);
+                            new CountDownTimer(1800000/speed, 600000/speed) {
 
-                    public void onTick(long millisUntilFinished) {
+                                public void onTick(long millisUntilFinished) {
 //                        mTextField.setText("seconds remaining: " + millisUntilFinished / 1000);
-                    }
+                                }
 
-                    public void onFinish() {
+                                public void onFinish() {
+                                    onClick(pendulum);
+                                }
+                            }.start();
+                        }
+                    }.start();
 
-                    }
-                }.start();
+
+
 
 //
 //                pendulum.animate().rotation(pendulum.getRotation()+180).start();
